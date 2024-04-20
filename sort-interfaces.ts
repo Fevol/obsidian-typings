5fa3c602983a948a1a7fc95ac676e50a316645a1
Fevol <vernieuwefelix@gmail.com>
2024-04-20 03:04:49 +0200

    fix: support for multiple modules sorting style: format typings

--- sort-interfaces.ts
+++ sort-interfaces.ts
@@ -1,7 +1,7 @@
 /**
  * This script is very bad, verging on being completely broken.
  * Current problems:
- *   - Only keeps interfaces, types and classes inside the obsidian module
+ *   - Only keeps interfaces, types and classes inside modules
  *   - Sorting of interface properties and methods is quite slow
  *   - Probably improper re-setting of declaration structure for new ordering of properties and methods
  *   - Endlines between methods/properties, types/interfaces is quite bodgy
@@ -9,19 +9,17 @@
  */
 // @ts-nocheck (Prevent compilation errors for people use the package as a submodule)
 
-import {Project, Node} from "ts-morph";
+import {Project, Node, ModuleDeclaration, SourceFile} from "ts-morph";
 import * as fs from "node:fs";
 
 const project = new Project();
 const declarationsToText = (declarationArray: Node[]) => declarationArray.map(declaration => declaration.getText(true)).join('\n\n');
 const sortFn = (a: any, b: any) => a.getName().localeCompare(b.getName());
 
-async function parseFile(file: string, output_file: string = file) {
-    const sourceFile = project.addSourceFileAtPath(file);
-    const obsidianDeclaration = sourceFile.getModules()[0];
 
-    const typeDeclarations = obsidianDeclaration.getTypeAliases().sort(sortFn);
-    const interfaceDeclarations = [...obsidianDeclaration.getClasses(), ...obsidianDeclaration.getInterfaces()].sort(sortFn);
+async function sortModule(module: ModuleDeclaration, file: SourceFile) {
+    const typeDeclarations = module.getTypeAliases().sort(sortFn);
+    const interfaceDeclarations = [...module.getClasses(), ...module.getInterfaces()].sort(sortFn);
     for (const declaration of interfaceDeclarations) {
         const structure = declaration.getStructure();
         // NOTE: This is pretty slow, not sure what the proper way to sort properties would be
@@ -37,20 +35,30 @@ async function parseFile(file: string, output_file: string = file) {
             declaration.insertText(declaration.getMethods()[0].getStart(true), '\n');
     }
 
-    const newFile = project.createSourceFile(file === output_file ? "temp.d.ts" : output_file, '', {overwrite: true});
-
-    const nodesBeforeModule = obsidianDeclaration.getPreviousSiblings().reverse();
-    nodesBeforeModule.forEach(node => newFile.addStatements(node.getText()));
-
-    const docs = obsidianDeclaration.getJsDocs().map(jsDoc => jsDoc.getText()).join('\n');
-    newFile.addStatements(docs);
-
-    const newModule = newFile.addModule({ name: obsidianDeclaration.getName() });
+    const newModule = file.addModule({
+        name: module.getName(),
+        docs: module.getJsDocs().map((getJsDoc) => getJsDoc.getStructure())
+    });
 
     // Apparently I can't just add an endline to the end, nor use a writer to add an endline at the end, hence the comment
-    newModule.addStatements(declarationsToText(typeDeclarations) + " \n\n// Interfaces \n");
+    if (typeDeclarations.length)
+        newModule.addStatements(declarationsToText(typeDeclarations) + " \n\n// Interfaces \n");
     newModule.addStatements(declarationsToText(interfaceDeclarations));
+}
 
+async function parseFile(file: string, output_file: string = file) {
+    const sourceFile = project.addSourceFileAtPath(file);
+
+    const newFile = project.createSourceFile(file === output_file ? "temp.d.ts" : output_file, '', {overwrite: true});
+    
+    const imports = sourceFile.getImportDeclarations().map(declaration => declaration.getText());
+    const exports = sourceFile.getExportDeclarations().map(declaration => declaration.getText());
+    newFile.addStatements(imports);
+    newFile.addStatements(exports);
+    
+    for (const module of sourceFile.getModules())
+        await sortModule(module, newFile);
+    
     await newFile.save()
 
     if (file === output_file)
