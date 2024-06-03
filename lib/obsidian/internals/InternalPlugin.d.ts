d17c8c8e8b77ec6e8b90b41a93d6f209d51aa3db
Michael Naumov <mnaoumov@gmail.com>
2024-06-02 22:15:35 -0600

    Move InternalPlugins

--- lib/obsidian/internals/InternalPlugin.d.ts
+++ lib/obsidian/internals/InternalPlugin.d.ts
@@ -1,4 +1,5 @@
 import InternalPluginName from "./InternalPluginName.ts";
+import type Events from "../Events.d.ts";
 import type TFile from "../TFile.d.ts";
 import type TFolder from "../TFolder.d.ts";
 import type Plugin from "../Plugin.d.ts";
@@ -114,3 +115,57 @@ interface WordCountPluginInstance extends InternalPluginInstance { }
 interface WorkspacesPluginInstance extends InternalPluginInstance { }
 
 interface ZkPrefixerPluginInstance extends InternalPluginInstance { }
+
+interface InternalPlugins extends Events {
+    /**
+     * Reference to App
+     */
+    app: App;
+    /**
+     * Mapping of whether an internal plugin is enabled
+     */
+    config: Record<InternalPluginName, boolean>;
+    /** @internal */
+    migration: boolean;
+    /**
+     * Plugin configs for internal plugins
+     *
+     * @remark Prefer usage of getPluginById to access a plugin
+     */
+    plugins: {
+        [InternalPluginNameValue in keyof InternalPluginNameInstancesMapping]: InternalPlugin<
+            InternalPluginNameInstancesMapping[InternalPluginNameValue]
+        >;
+    };
+
+    /** @internal - Load plugin configs and enable plugins */
+    enable(): Promise<void>;
+    /**
+     * Get an enabled internal plugin by ID
+     *
+     * @param id - ID of the plugin to get
+     */
+    getEnabledPluginById<ID extends InternalPluginName>(
+        id: ID
+    ): InternalPluginNameInstancesMapping[ID] | null;
+    /**
+     * Get all enabled internal plugins
+     */
+    getEnabledPlugins(): InternalPlugin[];
+    /**
+     * Get an internal plugin by ID
+     *
+     * @param id - ID of the plugin to get
+     */
+    getPluginById<ID extends InternalPluginName>(
+        id: ID
+    ): InternalPlugin<InternalPluginNameInstancesMapping[ID]> | null;
+    /** @internal */
+    loadPlugin(arg: { id: string; name: string; }): string;
+    /** @internal */
+    onRaw(cb1: unknown, cb2: unknown): void;
+    /** @internal Request save of plugin configs */
+    requestSaveConfig(): void;
+    /** @internal - Save current plugin configs */
+    saveConfig(): Promise<void>;
+}
