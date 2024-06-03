ae5ed31798293f57753ce10728834486001983ac
Michael Naumov <mnaoumov@gmail.com>
2024-06-02 22:19:46 -0600

    Use node namespace

--- lib/obsidian/DataAdapter.d.ts
+++ lib/obsidian/DataAdapter.d.ts
@@ -1,7 +1,7 @@
 import { IpcRenderer } from "electron";
-import * as fs from "fs";
-import * as fsPromises from "fs/promises";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as fsPromises from "node:fs/promises";
+import * as path from "node:path";
 import { FileEntry } from "./FileEntry.d.ts";
 
 /** @todo Documentation incomplete */
