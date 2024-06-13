659f2a50ad66dc54bbb342aea225499e889989ca
Michael Naumov <mnaoumov@gmail.com>
2024-06-12 23:51:31 -0600

    Extend Platform

--- lib/obsidian/augmentations/Platform.d.ts
+++ lib/obsidian/augmentations/Platform.d.ts
@@ -1,5 +1,14 @@
 export {};
 
 declare module "obsidian" {
-    interface Platform {}
+    interface Platform {
+        canExportPdf: boolean;
+        canPopoutWindow: boolean;
+        canSplit: boolean;
+        canStackTabs: boolean;
+        isLinux: boolean;
+        mobileDeviceHeight: number;
+        mobileKeyboardHeight: number;
+        mobileSoftKeyboardVisible: boolean;
+    }
 }
