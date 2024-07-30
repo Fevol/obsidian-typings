3c121145748788b36fec47b15b1c3a632d1b0fbc
Michael Naumov <mnaoumov@gmail.com>
2024-07-30 18:30:53 +0300

    feat: add createTFile/TFolder functions (#71)

--- src/implementations/index.ts
+++ src/implementations/index.ts
@@ -1,2 +1,4 @@
+export { createTFile } from "./createTFile.ts";
+export { createTFolder } from "./createTFolder.ts";
 export * from "./CustomArrayDictImpl.ts";
 export * from "./InternalPluginName.ts";
