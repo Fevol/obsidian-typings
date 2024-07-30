4f42a3d7082909b8716a48f2ed56b8d5cae3378e
Michael Naumov <mnaoumov@gmail.com>
2024-07-30 00:17:52 -0600

    More scripts

--- scripts/build-clean.ts
+++ scripts/build-clean.ts
@@ -1,2 +1,9 @@
-import { rm } from "node:fs/promises";
+import { rm, readdir } from "node:fs/promises";
 await rm("dist", { recursive: true, force: true });
+
+const files = await readdir("src/implementations");
+const dtsFiles = files.filter(file => file.endsWith(".d.ts"));
+
+for (const file of dtsFiles) {
+    await rm(`src/implementations/${file}`);
+}
