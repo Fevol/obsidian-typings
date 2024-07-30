2631257dca84957f4b2cb71849b456ef16c01299
Michael Naumov <mnaoumov@gmail.com>
2024-07-30 09:04:17 -0600

    Remove isLinux as it is in official API

--- src/obsidian/augmentations/Platform.d.ts
+++ src/obsidian/augmentations/Platform.d.ts
@@ -6,7 +6,6 @@ declare module "obsidian" {
         canPopoutWindow: boolean;
         canSplit: boolean;
         canStackTabs: boolean;
-        isLinux: boolean;
         mobileDeviceHeight: number;
         mobileKeyboardHeight: number;
         mobileSoftKeyboardVisible: boolean;
