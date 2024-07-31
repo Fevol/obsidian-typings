bdd8b50472b248bc774e60f31da65d092efcd641
Michael Naumov <mnaoumov@gmail.com>
2024-07-31 16:32:33 -0600

    Fix build

--- src/obsidian/augmentations/FootnoteCache.d.ts
+++ src/obsidian/augmentations/FootnoteCache.d.ts
@@ -1,6 +1,10 @@
-/**
- * Cache item containing a notes footnotes metadata
- */
-export interface FootnoteCache extends CacheItem {
-    identifier: string;
+export {};
+
+declare module "obsidian" {
+    /**
+     * Cache item containing a notes footnotes metadata
+     */
+    interface FootnoteCache extends CacheItem {
+        identifier: string;
+    }
 }
