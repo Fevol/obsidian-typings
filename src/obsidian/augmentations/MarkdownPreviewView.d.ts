a74087ca7dec733bb17ada6e21819098bf778596
4Source <38220764+4Source@users.noreply.github.com>
2024-08-03 13:02:36 +0200

    Extends for MarkdownRenderer, MarkdownRenderChild, MarkdownPreviewView

--- src/obsidian/augmentations/MarkdownPreviewView.d.ts
+++ src/obsidian/augmentations/MarkdownPreviewView.d.ts
@@ -4,7 +4,7 @@ export {};
 
 declare module "obsidian" {
     /** @todo Documentation incomplete */
-    interface MarkdownPreviewView {
+    interface MarkdownPreviewView extends MarkdownRenderer{
         /**
          * Unique identifier for the rendered element
          */
@@ -19,22 +19,35 @@ declare module "obsidian" {
 
         applyFoldInfo(e: unknown): unknown;
         beforeUnload(): unknown;
+		/**
+		 * Clears the rendered preview
+		 */
         clear(): unknown;
         edit(e: unknown): unknown;
         foldAll(): unknown;
         getEphemeralState(e: unknown): unknown;
         getFoldInfo(): unknown;
         getSelection(): unknown;
-        hide(): unknown;
+        hide(): void;
         onFoldChange(): unknown;
         onRenderComplete(): unknown;
         onResize(): unknown;
         onScroll(): unknown;
         requestUpdateLinks(): unknown;
         setEphemeralState(e: unknown): unknown;
-        show(): unknown;
+        show(): void;
         showSearch(): unknown;
         unfoldAll(): unknown;
         updateOptions(): unknown;
+		/**
+		 * Returns the renderer text
+		 */
+		get(): string;
+		/**
+		 * Set the Preview to new data
+		 * @param data The data to set the renderer to
+		 * @param clear If the preview should be cleared
+		 */
+		set(data: string, clear: boolean): void;
     }
 }
