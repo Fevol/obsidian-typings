186d5134289baad73bd03d032aa8388b5a8f5e79
Michael Naumov <mnaoumov@gmail.com>
2024-08-03 17:07:27 -0600

    chore: remove index

--- scripts/build-clean.ts
+++ scripts/build-clean.ts
@@ -10,3 +10,6 @@ const dtsFiles = files.filter(file => file.endsWith(".d.ts"));
 for (const file of dtsFiles) {
     await rm(`src/obsidian/implementations/${file}`);
 }
+
+await rm("src/index.d.ts", { force: true });
+await rm("src/obsidian/implementations/index.ts", { force: true });
