7c2a039300c6b488b2615ca464be99d93e925798
Michael Naumov <mnaoumov@gmail.com>
2024-08-17 18:06:03 -0600

    fix: ensure style-mod.d.cts is loaded

--- scripts/build-style-mod.ts
+++ scripts/build-style-mod.ts
@@ -1,3 +1,7 @@
-import { cp } from "node:fs/promises";
+import {
+    appendFile,
+    cp
+} from "node:fs/promises";
 
 await cp("src/style-mod/style-mod.d.cts.dist", "dist/style-mod.d.cts");
+await appendFile("dist/types.d.ts", "import \"./style-mod.d.cts\";\n");
