ae1786b292f26fc1be6bc111ea225049160c49f7
Michael Naumov <mnaoumov@gmail.com>
2024-09-07 20:29:56 -0600

    feat: TFolder.getParentPrefix

--- src/obsidian/augmentations/TFolder.d.ts
+++ src/obsidian/augmentations/TFolder.d.ts
@@ -1,5 +1,14 @@
 export {};
 
 declare module "obsidian" {
-    interface TFolder extends TAbstractFile {}
+    interface TFolder extends TAbstractFile {
+        /**
+         * Returns the prefix of the folder path.
+         * If the folder is in the root '/', it returns an empty string.
+         * If the folder is 'a/b/c', it returns 'a/b/'.
+         *
+         * @returns The prefix of the folder.
+         */
+        getParentPrefix(): string;
+    }
 }
