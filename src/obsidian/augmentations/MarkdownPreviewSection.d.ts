12b7a128a6963abe9cda24248582e08e15806b1d
Michael Naumov <mnaoumov@gmail.com>
2024-09-15 22:50:29 -0600

    feat: MarkdownPreviewSection

--- src/obsidian/augmentations/MarkdownPreviewSection.d.ts
+++ src/obsidian/augmentations/MarkdownPreviewSection.d.ts
@@ -1,5 +1,24 @@
 export {};
 
 declare module 'obsidian' {
-    interface MarkdownPreviewSection {}
+    interface MarkdownPreviewSection {
+        computed: boolean;
+        el: HTMLDivElement;
+        headingCollapsed: boolean;
+        height: number;
+        highlightRanges: null;
+        html: string;
+        level: number;
+        lineEnd: number;
+        lineStart: number
+        lines: number;
+        rendered: boolean;
+        shown: boolean;
+        used: boolean;
+        usesFrontMatter: boolean;
+
+        render(): void;
+        resetCompute(): void;
+        setCollapsed(collapsed: boolean): void;
+    }
 }
