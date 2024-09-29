344cbe5e2538569f46251ffcbcdfe94ffac91d45
Michael Naumov <mnaoumov@gmail.com>
2024-09-28 20:07:48 -0600

    feat: BacklinkPluginInstance

--- src/obsidian/internals/InternalPlugin/BacklinkPluginInstance.d.ts
+++ src/obsidian/internals/InternalPlugin/BacklinkPluginInstance.d.ts
@@ -1,4 +1,4 @@
-import type { App } from 'obsidian';
+import type { App, Menu, TAbstractFile, TFile, WorkspaceLeaf } from 'obsidian';
 import type { BacklinkPlugin } from './BacklinkPlugin.js';
 import type { InternalPluginInstance } from './InternalPluginInstance.js';
 
@@ -6,5 +6,21 @@ import type { InternalPluginInstance } from './InternalPluginInstance.js';
 export interface BacklinkPluginInstance extends InternalPluginInstance<BacklinkPlugin> {
     app: App;
     defaultOn: boolean;
+    file?: TFile | null;
     plugin: BacklinkPlugin;
+    options: {
+        backlinkInDocument?: boolean;
+    };
+
+    initLeaf(): void;
+    onEnable(app: App, plugin: BacklinkPlugin): Promise<void>;
+    onExternalSettingsChange(): Promise<void>;
+    onFileMenu(menu: Menu, file: TAbstractFile, title: string, leaf?: WorkspaceLeaf): Menu;
+    onFileOpen(file: TAbstractFile): void;
+    onUserDisable(app: App): void;
+    onUserEnable(): void;
+    openBacklinksForActiveFile(skipSplit: boolean): boolean | undefined;
+    toggleBacklinksInDocument(skip: boolean): boolean | undefined;
+    updateBacklinks(): void;
+
 }
