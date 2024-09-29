35265a452ced8956dffa5e8084f768be96c42955
Michael Naumov <mnaoumov@gmail.com>
2024-09-28 20:19:40 -0600

    feat: bookmarks

--- src/obsidian/internals/InternalPlugin/BookmarksPlugin.d.ts
+++ src/obsidian/internals/InternalPlugin/BookmarksPlugin.d.ts
@@ -1,5 +1,11 @@
+import type { WorkspaceLeaf } from 'obsidian';
+import type { BookmarksView } from '../Views/BookmarksView.js';
 import type { BookmarksPluginInstance } from './BookmarksPluginInstance.js';
 import type { InternalPlugin } from './InternalPlugin.js';
 
 /** @public */
-export interface BookmarksPlugin extends InternalPlugin<BookmarksPluginInstance> {}
+export interface BookmarksPlugin extends InternalPlugin<BookmarksPluginInstance> {
+    views: {
+        bookmarks(left: WorkspaceLeaf): BookmarksView;
+    };
+}
--- src/obsidian/internals/InternalPlugin/BookmarksPluginInstance.d.ts
+++ src/obsidian/internals/InternalPlugin/BookmarksPluginInstance.d.ts
@@ -1,10 +1,24 @@
-import type { App } from 'obsidian';
+import type {
+    App,
+    Debouncer,
+    Events
+} from 'obsidian';
 import type { BookmarksPlugin } from './BookmarksPlugin.js';
 import type { InternalPluginInstance } from './InternalPluginInstance.js';
 
 /** @public */
-export interface BookmarksPluginInstance extends InternalPluginInstance<BookmarksPlugin> {
+export interface BookmarksPluginInstance extends InternalPluginInstance<BookmarksPlugin>, Events {
     app: App;
     defaultOn: boolean;
     plugin: BookmarksPlugin;
+
+    bookmarkedViews: Map<unknown, unknown>;
+    bookmarkLookup: Object;
+    description: string;
+    hasValidData: boolean;
+    id: string;
+    items: unknown[];
+    name: string;
+    urlBookmarkLookup: Object;
+    onItemsChanged: Debouncer<[], unknown>;
 }
