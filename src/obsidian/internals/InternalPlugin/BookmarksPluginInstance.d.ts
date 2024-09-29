5309df581b018289e18deaa9fe1a15b94faf478e
Michael Naumov <mnaoumov@gmail.com>
2024-09-29 17:48:22 -0600

    feat: Bookmark

--- src/obsidian/internals/InternalPlugin/BookmarksPluginInstance.d.ts
+++ src/obsidian/internals/InternalPlugin/BookmarksPluginInstance.d.ts
@@ -7,31 +7,34 @@ import type {
     MarkdownFileInfo,
     MarkdownView,
     Menu,
+    PaneType,
     TAbstractFile,
     TFile,
-    WorkspaceLeaf
+    View,
+    WorkspaceLeaf,
+    WorkspaceTabs
 } from 'obsidian';
 import type { BookmarkItem } from './BookmarkItem.js';
 import type { BookmarksPlugin } from './BookmarksPlugin.js';
 import type { InternalPluginInstance } from './InternalPluginInstance.js';
+import type { GlobalSearchLeaf } from '../GlobalSearchLeaf.js';
 
 /** @public */
 export interface BookmarksPluginInstance extends InternalPluginInstance<BookmarksPlugin>, Events {
     app: App;
-    bookmarkedViews: WeakMap<object, unknown>;
-    bookmarkLookup: Record<string, unknown>;
+    bookmarkedViews: WeakMap<View, HTMLElement>;
+    bookmarkLookup: Record<string, BookmarkItem>;
     defaultOn: boolean;
     hasValidData: boolean;
     items: BookmarkItem[];
-    onItemsChanged: Debouncer<[boolean], unknown>;
+    onItemsChanged: Debouncer<[boolean], void>;
     plugin: BookmarksPlugin;
-    urlBookmarkLookup: Record<string, unknown>;
+    urlBookmarkLookup: Record<string, BookmarkItem>;
 
     _onItemsChanged(arg1: boolean): void;
     addItem(item: BookmarkItem, instance: BookmarksPluginInstance): void;
-    constructor(): unknown;
     editItem(item: BookmarkItem): void;
-    findBookmarkByView(view: FileView): unknown;
+    findBookmarkByView(view: FileView): BookmarkItem | null | undefined;
     getBookmarks(): BookmarkItem[];
     getItemTitle(item: BookmarkItem): string;
     initLeaf(): void;
@@ -43,15 +46,15 @@ export interface BookmarksPluginInstance extends InternalPluginInstance<Bookmark
     onFileMenu(menu: Menu, files: TAbstractFile[], source: string, leaf?: WorkspaceLeaf): void;
     onFileRename(file: TFile, oldPath: string): void;
     onFilesMenu(menu: Menu, files: TAbstractFile[], source: string, leaf?: WorkspaceLeaf): void;
-    onLeafMenu(arg1: unknown, arg2: unknown): unknown;
-    onSearchResultsMenu(arg1: unknown, arg2: unknown): unknown;
-    onTabGroupMenu(menu: Menu, arg2: unknown): unknown;
+    onLeafMenu(menu: Menu, leaf: WorkspaceLeaf): void;
+    onSearchResultsMenu(menu: Menu, search: GlobalSearchLeaf): void;
+    onTabGroupMenu(menu: Menu, tabsLeaf: WorkspaceTabs): void;
     onUserEnable(): void;
-    openBookmark(arg1: unknown, arg2: unknown, arg3: unknown): Promise<unknown>;
-    openBookmarkInLeaf(arg1: unknown, arg2: unknown, arg3: unknown): Promise<unknown>;
-    openBookmarks(arg1: unknown, arg2: unknown): Promise<unknown>;
-    rebuildBookmarkCache(): unknown;
+    openBookmark(item: BookmarkItem, newLeaf: PaneType | boolean, newLeaf2?: PaneType | boolean): Promise<void>;
+    openBookmarkInLeaf(item: BookmarkItem, leaf: WorkspaceLeaf, newLeaf?: PaneType | boolean): Promise<void>;
+    openBookmarks(items: BookmarkItem[], newLeaf?: PaneType | boolean): Promise<void>;
+    rebuildBookmarkCache(): void;
     removeItem(item: BookmarkItem): void;
-    saveData(): unknown;
+    saveData(): void;
     updateTabHeaders(): void;
 }
