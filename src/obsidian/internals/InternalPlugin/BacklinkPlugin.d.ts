d590375f7ec021948f1912b45d9550cc1bd2bd9e
Michael Naumov <mnaoumov@gmail.com>
2024-09-28 20:19:19 -0600

    chore: change type

--- src/obsidian/internals/InternalPlugin/BacklinkPlugin.d.ts
+++ src/obsidian/internals/InternalPlugin/BacklinkPlugin.d.ts
@@ -1,11 +1,11 @@
 import type { WorkspaceLeaf } from 'obsidian';
+import type { BacklinkView } from '../Views/BacklinkView.js';
 import type { BacklinkPluginInstance } from './BacklinkPluginInstance.js';
 import type { InternalPlugin } from './InternalPlugin.js';
-import type { BacklinkView } from '../Views/BacklinkView.js';
 
 /** @public */
 export interface BacklinkPlugin extends InternalPlugin<BacklinkPluginInstance> {
     views: {
-        backlink: (left: WorkspaceLeaf) => BacklinkView;
-    }
+        backlink(left: WorkspaceLeaf): BacklinkView;
+    };
 }
