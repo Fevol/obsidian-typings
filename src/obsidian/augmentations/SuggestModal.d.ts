512043fbf0da8499d737f4919d38f35b09454b4e
Michael Naumov <mnaoumov@gmail.com>
2024-10-08 17:48:22 -0600

    chore: reformat

--- src/obsidian/augmentations/SuggestModal.d.ts
+++ src/obsidian/augmentations/SuggestModal.d.ts
@@ -3,8 +3,8 @@ export {};
 declare module 'obsidian' {
     /** @todo Documentation incomplete */
     interface SuggestModal<T> extends Modal, ISuggestOwner<T> {
-        ctaEl: HTMLDivElement;
         /** @internal */
         clearButtonEl: HTMLButtonElement;
+        ctaEl: HTMLDivElement;
     }
 }
