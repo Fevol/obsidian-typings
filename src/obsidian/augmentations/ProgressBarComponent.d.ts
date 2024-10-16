91e0c617012d1941f3b10d8ec0d8035036b132c7
Michael Naumov <mnaoumov@gmail.com>
2024-10-15 18:50:24 -0600

    chore: reformat

--- src/obsidian/augmentations/ProgressBarComponent.d.ts
+++ src/obsidian/augmentations/ProgressBarComponent.d.ts
@@ -3,14 +3,13 @@ export {};
 declare module 'obsidian' {
     interface ProgressBarComponent extends ValueComponent<number> {
         /**
-         * Access the "bar" element
-         */
-        progressBar: HTMLDivElement;
-
-        /**
          * Access the "line" element which is a child of the progressBar element
          */
         lineEl: HTMLDivElement;
+        /**
+         * Access the "bar" element
+         */
+        progressBar: HTMLDivElement;
 
         /**
          * Shows/hides the component
