8578d4ae70dfa0870c050c11fd279175ced4add1
Michael Naumov <mnaoumov@gmail.com>
2024-10-15 18:40:55 -0600

    feat: add method

--- src/obsidian/augmentations/ProgressBarComponent.d.ts
+++ src/obsidian/augmentations/ProgressBarComponent.d.ts
@@ -11,5 +11,13 @@ declare module 'obsidian' {
          * Access the "line" element which is a child of the progressBar element
          */
         lineEl: HTMLDivElement;
+
+        /**
+         * Shows/hides the component
+         *
+         * @param visible Whether the setting should be visible
+         * @returns The component
+         */
+        setVisibility(visible: boolean): this;
     }
 }
