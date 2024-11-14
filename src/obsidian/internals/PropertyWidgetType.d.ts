ba7e4a58fb3d4ac2b8e69778d7fa5a191a670faa
unxok <anderluke707@gmail.com>
2024-11-13 16:49:31 -0800

    fixed def for PropertyWidgetType

--- src/obsidian/internals/PropertyWidgetType.d.ts
+++ src/obsidian/internals/PropertyWidgetType.d.ts
@@ -1,14 +1,14 @@
-/**
- * @todo Documentation incomplete
- */
 /** @public */
-export type PropertyWidgetType =
-    | 'aliases'
-    | 'checkbox'
-    | 'date'
-    | 'datetime'
-    | 'multitext'
-    | 'number'
-    | 'tags'
-    | 'text'
-    | (string & any);
+export type PropertyWidgetType = {
+    name: string;
+    type:
+        | 'aliases'
+        | 'checkbox'
+        | 'date'
+        | 'datetime'
+        | 'multitext'
+        | 'number'
+        | 'tags'
+        | 'text'
+        | string;
+};
