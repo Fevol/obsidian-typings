f0ccd528fd3f560e9f8dfaabe3f553b6e77dc402
Michael Naumov <mnaoumov@gmail.com>
2024-12-08 18:00:38 -0600

    feat: add optional plugin functions

--- src/obsidian/internals/InternalPlugin/InternalPluginInstance.d.ts
+++ src/obsidian/internals/InternalPlugin/InternalPluginInstance.d.ts
@@ -5,6 +5,10 @@ export interface InternalPluginInstance<InternalPlugin> {
     description: string;
     id: string;
     name: string;
+    onDisable?: (app: App, plugin: InternalPlugin) => void;
+    onEnable?: (app: App, plugin: InternalPlugin) => Promise<void>;
+    onUserDisable?: (app: App) => void;
+    onUserEnable?: (app: App) => void;
 
     init(app: App, plugin: InternalPlugin): void;
 }
