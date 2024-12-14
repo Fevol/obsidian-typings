a8c80e153a6415470ff03105610d97bc0c1a7248
Michael Naumov <mnaoumov@gmail.com>
2024-12-13 23:18:44 -0600

    feat: fill ViewRegistryViewByTypeRecord

--- src/obsidian/internals/ViewRegistry/ViewRegistryViewByTypeRecord.d.ts
+++ src/obsidian/internals/ViewRegistry/ViewRegistryViewByTypeRecord.d.ts
@@ -1,6 +1,14 @@
 import type {
     ViewCreator
 } from 'obsidian';
+import type { TypedViewCreator } from '../Views/TypedViewCreator.js';
+import type { ViewTypeViewMapping } from '../Views/ViewTypeViewMapping.js';
+import type { ViewTypeType } from '../Views/ViewTypeType.js';
 
 /** @public */
-export interface ViewRegistryViewByTypeRecord extends Record<string, ViewCreator> {}
+export interface ViewRegistryViewByTypeRecord extends Record<string, ViewCreator>, Mapping {
+}
+
+type Mapping = {
+    [TViewType in ViewTypeType]: TypedViewCreator<ViewTypeViewMapping[TViewType]>
+};
