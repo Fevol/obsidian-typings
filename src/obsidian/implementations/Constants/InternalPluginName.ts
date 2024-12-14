2d15242b09acdc3a4c10219add4f5a2863e49e0c
Michael Naumov <mnaoumov@gmail.com>
2024-12-13 22:40:05 -0600

    feat: add UI Plugin Names

--- src/obsidian/implementations/Constants/InternalPluginName.ts
+++ src/obsidian/implementations/Constants/InternalPluginName.ts
@@ -1,31 +1,120 @@
 /** @public */
 export const InternalPluginName = {
+    /**
+     * Plugin name in UI: Audio recorder
+     */
     AudioRecorder: 'audio-recorder',
+    /**
+     * Plugin name in UI: Backlinks
+     */
     Backlink: 'backlink',
+    /**
+     * Plugin name in UI: Bookmarks
+     */
     Bookmarks: 'bookmarks',
+    /**
+     * Plugin name in UI: Web viewer
+     * @remark Currently disabled
+     */
+    Browser: 'browser',
+    /**
+     * Plugin name in UI: Canvas
+     */
     Canvas: 'canvas',
+    /**
+     * Plugin name in UI: Command palette
+     */
     CommandPalette: 'command-palette',
+    /**
+     * Plugin name in UI: Daily notes
+     */
     DailyNotes: 'daily-notes',
+    /**
+     * Plugin name in UI: (hidden)
+     */
     EditorStatus: 'editor-status',
+    /**
+     * Plugin name in UI: Files
+     */
     FileExplorer: 'file-explorer',
+    /**
+     * Plugin name in UI: File recovery
+     */
     FileRecovery: 'file-recovery',
+    /**
+     * Plugin name in UI: Search
+     */
     GlobalSearch: 'global-search',
+    /**
+     * Plugin name in UI: Graph view
+     */
     Graph: 'graph',
+    /**
+     * Plugin name in UI: Format converter
+     */
     MarkdownImporter: 'markdown-importer',
+    /**
+     * Plugin name in UI: Note composer
+     */
     NoteComposer: 'note-composer',
+    /**
+     * Plugin name in UI: Outgoing links
+     */
     OutgoingLink: 'outgoing-link',
+    /**
+     * Plugin name in UI: Outline
+     */
     Outline: 'outline',
+    /**
+     * Plugin name in UI: Page preview
+     */
     PagePreview: 'page-preview',
+    /**
+     * Plugin name in UI: Properties view
+     */
     Properties: 'properties',
+    /**
+     * Plugin name in UI: Publish
+     */
     Publish: 'publish',
+    /**
+     * Plugin name in UI: Random note
+     */
     RandomNote: 'random-note',
+    /**
+     * Plugin name in UI: Slash commands
+     */
     SlashCommand: 'slash-command',
+    /**
+     * Plugin name in UI: Slides
+     */
     Slides: 'slides',
+    /**
+     * Plugin name in UI: Quick Switcher
+     */
     Switcher: 'switcher',
+    /**
+     * Plugin name in UI: Sync
+     */
     Sync: 'sync',
+    /**
+     * Plugin name in UI: Tags view
+     */
     TagPane: 'tag-pane',
+    /**
+     * Plugin name in UI: Templates
+     */
     Templates: 'templates',
+    /**
+     * Plugin name in UI: Word count
+     */
     WordCount: 'word-count',
+    /**
+     * Plugin name in UI: Workspaces
+     */
     Workspaces: 'workspaces',
+    /**
+     * Plugin name in UI: Unique note creator
+     */
     ZkPrefixer: 'zk-prefixer'
 } as const;
