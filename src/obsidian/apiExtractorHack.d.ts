dc5fb5514c6e4c09951103ab75560f2a196c0871
Michael Naumov <mnaoumov@gmail.com>
2024-12-13 23:45:56 -0600

    chore: include all implementations types in the bundle

--- src/obsidian/apiExtractorHack.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file is used to ensure that API Extractor does not complain about missing exports.
- *
- * Add corresponding export to this file if API Extractor complains about missing exports.
- * Warning: (...) - (ae-forgotten-export) The symbol "(...)" needs to be exported by the entry point index.d.ts
- */
-
-export { InternalPluginName } from './implementations/Constants/InternalPluginName.js';
-export { ViewType } from './implementations/Constants/ViewType.js';
