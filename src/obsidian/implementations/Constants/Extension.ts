f3f8efbe6a0bb179b0a1a7c7bbb4ff6380440d8e
Michael Naumov <mnaoumov@gmail.com>
2024-12-13 22:17:42 -0600

    feat: add ViewRegistryTypeByExtensionRecord

---
+++ src/obsidian/implementations/Constants/Extension.ts
@@ -0,0 +1,27 @@
+/** @public */
+export const Extension = {
+    _3gp: '3gp',
+    avif: 'avif',
+    bmp: 'bmp',
+    canvas: 'canvas',
+    flac: 'flac',
+    gif: 'gif',
+    jpeg: 'jpeg',
+    jpg: 'jpg',
+    m4a: 'm4a',
+    md: 'md',
+    mkv: 'mkv',
+    mov: 'mov',
+    mp3: 'mp3',
+    mp4: 'mp4',
+    oga: 'oga',
+    ogg: 'ogg',
+    ogv: 'ogv',
+    opus: 'opus',
+    pdf: 'pdf',
+    png: 'png',
+    svg: 'svg',
+    wav: 'wav',
+    webm: 'webm',
+    webp: 'webp'
+} as const;
--- src/obsidian/internals/ViewRegistry/ViewRegistryTypeByExtensionRecord.d.ts
+++ src/obsidian/internals/ViewRegistry/ViewRegistryTypeByExtensionRecord.d.ts
@@ -1,2 +1,30 @@
+import type { Extension } from "../../implementations/Constants/Extension.ts";
+import type { ViewType } from "../../implementations/Constants/ViewType.ts";
+
 /** @public */
-export interface ViewRegistryTypeByExtensionRecord extends Record<string, string> {}
+export interface ViewRegistryTypeByExtensionRecord extends Record<string, string> {
+    [Extension._3gp]: typeof ViewType.Audio;
+    [Extension.avif]: typeof ViewType.Image;
+    [Extension.bmp]: typeof ViewType.Image;
+    [Extension.canvas]: typeof ViewType.Canvas;
+    [Extension.flac]: typeof ViewType.Audio;
+    [Extension.gif]: typeof ViewType.Image;
+    [Extension.jpeg]: typeof ViewType.Image;
+    [Extension.jpg]: typeof ViewType.Image;
+    [Extension.m4a]: typeof ViewType.Audio;
+    [Extension.md]: typeof ViewType.Markdown;
+    [Extension.mkv]: typeof ViewType.Video;
+    [Extension.mov]: typeof ViewType.Video;
+    [Extension.mp3]: typeof ViewType.Audio;
+    [Extension.mp4]: typeof ViewType.Video;
+    [Extension.oga]: typeof ViewType.Audio;
+    [Extension.ogg]: typeof ViewType.Audio;
+    [Extension.ogv]: typeof ViewType.Video;
+    [Extension.opus]: typeof ViewType.Audio;
+    [Extension.pdf]: typeof ViewType.Pdf;
+    [Extension.png]: typeof ViewType.Image;
+    [Extension.svg]: typeof ViewType.Image;
+    [Extension.wav]: typeof ViewType.Audio;
+    [Extension.webm]: typeof ViewType.Video;
+    [Extension.webp]: typeof ViewType.Image;
+}
