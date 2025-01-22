a9219881dffb115bc6524dd861c11c5ab2059e21
Fevol <8179397+fevol@users.noreply.github.com>
2025-01-22 16:45:47 +0100

    chore: bump to astro 5

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,9 +1,11 @@
 import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { defineCollection } from 'astro:content';
 import { pageThemeObsidianSchema } from 'starlight-theme-obsidian/schema';
 
 export const collections = {
     docs: defineCollection({
+        loader: docsLoader(),
         schema: docsSchema({
             extend: pageThemeObsidianSchema
         })
