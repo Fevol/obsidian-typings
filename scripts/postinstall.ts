44e36c7dbe6cc6a3daf1d0a710906128ff85c5da
Michael Naumov <mnaoumov@gmail.com>
2025-02-14 13:59:54 -0600

    chore: fix build

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,9 +1,5 @@
 import { spawnSync } from 'node:child_process';
 import {
-    join,
-    relative
-} from 'node:path/posix';
-import {
     cwd,
     env
 } from 'node:process';
@@ -14,10 +10,6 @@ const initCwd = env['INIT_CWD'];
 console.warn('initCwd', initCwd);
 console.warn('cwd', cwd());
 console.warn('env', JSON.stringify(env, null, 2));
-
-// const patchDir = relative(initCwd, join(cwd(), 'patches'));
-// console.warn('patchDir', patchDir);
-// console.warn('initCwd', initCwd);
 
 spawnSync('npx', ['patch-package', '--patch-dir', './patches'], {
     shell: true,
