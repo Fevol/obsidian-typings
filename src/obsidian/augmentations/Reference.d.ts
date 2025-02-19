eadf1732a18b7236c0acd5b1d263a5a7442eee78
Michael Naumov <mnaoumov@gmail.com>
2025-02-19 11:03:40 -0600

    fix: Reference
    see FrontmatterLinkCache instead

--- src/obsidian/augmentations/Reference.d.ts
+++ src/obsidian/augmentations/Reference.d.ts
@@ -2,7 +2,5 @@ export {};
 
 declare module 'obsidian' {
     interface Reference {
-        /** @internal dot delimetered, e.g., `symlink.0`, path in the frontmatter to the reference */
-        key?: string;
     }
 }
