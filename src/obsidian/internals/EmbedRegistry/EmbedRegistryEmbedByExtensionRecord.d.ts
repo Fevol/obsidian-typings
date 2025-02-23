c23da96ebe35967d8002744e4dea2b83d58ea837
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2025-02-23 18:22:45 +0400

    feat: improve EmbedRegistryEmbedByExtensionRecord type

--- src/obsidian/internals/EmbedRegistry/EmbedRegistryEmbedByExtensionRecord.d.ts
+++ src/obsidian/internals/EmbedRegistry/EmbedRegistryEmbedByExtensionRecord.d.ts
@@ -1,4 +1,4 @@
 import type { EmbeddableConstructor } from '../EmbeddableConstructor.d.ts';
 
 /** @public */
-export interface EmbedRegistryEmbedByExtensionRecord extends Record<string, EmbeddableConstructor> {}
+export interface EmbedRegistryEmbedByExtensionRecord extends Record<"md", EmbeddableConstructor>, Record<string, EmbeddableConstructor> {}
