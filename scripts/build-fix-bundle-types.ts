0c015cb0a6e4ff498ef798c7cff25a1f48ee8d71
Michael Naumov <mnaoumov@gmail.com>
2025-04-17 20:32:07 -0600

    chore: fix regex

--- scripts/build-fix-bundle-types.ts
+++ scripts/build-fix-bundle-types.ts
@@ -7,5 +7,5 @@ const DIST_TYPES_FILE = 'dist/cjs/types.d.cts';
 const content = await readFile(DIST_TYPES_FILE, 'utf-8');
 const newContent = content
     .replace(/Plugin\$1/g, 'Plugin')
-    .replace(/import \{\$1/g, 'import type {');
+    .replace(/import \{/g, 'import type {');
 await writeFile(DIST_TYPES_FILE, newContent, 'utf-8');
