ccc6bf6f7e6032af2f30547f1533f8f855af91eb
Michael Naumov <mnaoumov@gmail.com>
2025-04-20 11:45:51 -0600

    docs: more todo

--- src/i18next/augmentations/vars.d.ts
+++ src/i18next/augmentations/vars.d.ts
@@ -2,21 +2,28 @@ export {};
 
 declare module 'i18next' {
     /**
+     * @todo Documentation incomplete.
+     *
      * @unofficial
      */
     var isLanguageChangingTo: string | undefined;
 
     /**
+     * @todo Documentation incomplete.
      * @unofficial
      */
     var logger: unknown;
 
     /**
+     * @todo Documentation incomplete.
+     *
      * @unofficial
      */
     var observers: object;
 
     /**
+     * @todo Documentation incomplete.
+     *
      * @unofficial
      */
     var translator: unknown;
