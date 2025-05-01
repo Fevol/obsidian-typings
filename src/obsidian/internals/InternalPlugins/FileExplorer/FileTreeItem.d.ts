bbf18f3ebd4557b2b52af72056e2ca14b61703de
kotaindah55 <kotaindah55@gmail.com>
2025-05-02 00:38:09 +0300

    fix: `FileTreeItem` now implements `AbstractFileTreeItem` and `TreeItem`

--- src/obsidian/internals/InternalPlugins/FileExplorer/FileTreeItem.d.ts
+++ src/obsidian/internals/InternalPlugins/FileExplorer/FileTreeItem.d.ts
@@ -1,11 +1,21 @@
-import type { TAbstractFile } from 'obsidian';
+import type { TFile } from 'obsidian';
+import type { AbstractFileTreeItem } from './AbstractFileTreeItem.d.ts';
+import type { TreeItem } from '../../Tree/TreeItem.d.ts';
+import type { FileTreeItemParent } from './FileTreeItemParent.d.ts';
 
 /** @todo Documentation incomplete */
 /**
  * @public
  * @unofficial
  */
-export interface FileTreeItem {
-    /** @todo Documentation incomplete. */
-    file: TAbstractFile;
+export interface FileTreeItem extends AbstractFileTreeItem<TFile>, TreeItem<FileTreeItemParent> {
+    /**
+     * Element that indicates associated file extension,
+     * if it wasn't a Markdown file.
+     */
+    tagEl: HTMLElement | null;
+    /**
+     * @todo Documentation incomplete.
+     */
+    isSupported(): boolean;
 }
