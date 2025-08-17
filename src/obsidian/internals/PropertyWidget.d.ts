e1717ed5a2d23673cc685582b94c03f426a5bfdb
Michael Naumov <mnaoumov@gmail.com>
2025-08-16 22:43:09 -0600

    fix: remove deprecated in 1.9.0

--- src/obsidian/internals/PropertyWidget.d.ts
+++ src/obsidian/internals/PropertyWidget.d.ts
@@ -24,25 +24,12 @@ export interface PropertyWidget<Value = unknown, ComponentType extends Component
     type: string;
 
     /**
-     * Get the default value for the property widget.
-     * @deprecated Removed in 1.9.0.
-     */
-    default(): Value;
-
-    /**
      * Returns the I18N name of the widget.
      */
     name(): string;
 
     /**
      * Render function for the widget on field container given context and data.
-     * @deprecated Removed in 1.9.0.
-     */
-    render(containerEl: HTMLElement, data: PropertyEntryData<Value>, context: PropertyRenderContext): ComponentType;
-
-    /**
-     * Render function for the widget on field container given context and data.
-     * @remarks Added in 1.9.0.
      */
     render(containerEl: HTMLElement, data: Value, context: PropertyRenderContext): ComponentType;
 
