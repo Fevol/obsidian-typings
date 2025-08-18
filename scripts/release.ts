449b8853bdcb6fcc2099f0180e31092db515c16c
Michael Naumov <mnaoumov@gmail.com>
2025-08-17 22:47:32 -0600

    chore: add single release tag

---
+++ scripts/release.ts
@@ -0,0 +1,9 @@
+import { exec } from 'obsidian-dev-utils/ScriptUtils/Exec';
+import { wrapCliTask } from 'obsidian-dev-utils/ScriptUtils/CliUtils';
+import { checkGitRepoClean } from 'obsidian-dev-utils/ScriptUtils/version';
+
+await wrapCliTask(async () => {
+    await checkGitRepoClean();
+    await exec('git tag release-candidate');
+    await exec('git push origin --follow-tags');
+});
