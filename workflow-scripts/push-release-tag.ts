937e359d5791c2e67a905552a85b3987a55741c1
Michael Naumov <mnaoumov@gmail.com>
2025-08-21 15:47:43 -0600

    chore: force switching branch

--- workflow-scripts/push-release-tag.ts
+++ workflow-scripts/push-release-tag.ts
@@ -110,7 +110,7 @@ async function updateNpmVersion(nextVersion: string): Promise<void> {
 }
 
 async function updateNpmVersions(branchName: string, isBeta: boolean): Promise<string> {
-  await exec('git checkout main');
+  await exec('git checkout main --force');
   const packageJson = await readPackageJson();
   if (!packageJson.version) {
     throw new Error('package.json version is not set');
@@ -122,7 +122,7 @@ async function updateNpmVersions(branchName: string, isBeta: boolean): Promise<s
 
   await updateNpmVersion(nextVersion);
 
-  await exec(`git checkout ${branchName}`);
+  await exec(`git checkout ${branchName} --force`);
   await updateNpmVersion(nextVersion);
 
   await annotateTag(nextVersion, nextVersion);
