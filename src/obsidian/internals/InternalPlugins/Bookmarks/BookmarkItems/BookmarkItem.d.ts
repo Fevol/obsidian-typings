dbc01d701987ae20418b270a9c5ebcb7aaebb318
Michael Naumov <mnaoumov@gmail.com>
2025-08-28 22:59:32 -0600

    feat: BookmarkItem

--- src/obsidian/internals/InternalPlugins/Bookmarks/BookmarkItems/BookmarkItem.d.ts
+++ src/obsidian/internals/InternalPlugins/Bookmarks/BookmarkItems/BookmarkItem.d.ts
@@ -1,12 +1,47 @@
 /**
- * @todo Documentation incomplete.
+ * A bookmark item in the bookmarks plugin.
+ *
  * @public
  * @unofficial
  */
 export interface BookmarkItem {
-    /** @todo Documentation incomplete. */
+    /**
+     * The creation time of the bookmark item.
+     */
     ctime: number;
 
-    /** @todo Documentation incomplete. */
+    /**
+     * The items of the bookmark item.
+     */
+    items: BookmarkItem[];
+
+    /**
+     * The path of the bookmark item.
+     */
+    path?: string;
+
+    /**
+     * The query of the bookmark item.
+     */
+    query?: string;
+
+    /**
+     * The subpath of the bookmark item.
+     */
+    subpath?: string;
+
+    /**
+     * The title of the bookmark item.
+     */
+    title: string;
+
+    /**
+     * The type of the bookmark item.
+     */
     type: 'file' | 'folder' | 'group' | 'graph' | 'search' | 'url';
+
+    /**
+     * The URL of the bookmark item.
+     */
+    url?: string;
 }
