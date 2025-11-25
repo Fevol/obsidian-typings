890cde409bd2fb77e540456a58de12808f925998
Michael Naumov <mnaoumov@gmail.com>
2025-11-25 09:30:56 -0600

    fix: api-extractor warnings

--- src/obsidian/implementations/Functions/loadMermaid.ts
+++ src/obsidian/implementations/Functions/loadMermaid.ts
@@ -3,11 +3,13 @@ import { loadMermaid as loadMermaidOriginal } from 'obsidian';
 
 /**
  * Load Mermaid and return a promise to the global mermaid object.
- * Can also use {@link Window.mermaid} after this promise resolves to get the same reference.
+ * Can also use `window.mermaid` after this promise resolves to get the same reference.
  *
- * @returns A promise that resolves to the global {@link Window.mermaid} object.
+ * @returns A promise that resolves to the global `window.mermaid` object.
  *
- * @see {@link https://mermaid.js.org/ Official Mermaid documentation}.
+ * @see {@link https://mermaid.js.org/ | Official Mermaid documentation}.
+ * @public
+ * @unofficial
  */
 export async function loadMermaid(): Promise<Mermaid> {
     return (await loadMermaidOriginal()) as Mermaid;
