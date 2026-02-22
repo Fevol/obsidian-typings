4a907b788f0305fb3d31ef281577cdb160f2509a
Michael Naumov <mnaoumov@gmail.com>
2026-02-22 16:41:15 -0600

    fix: compilation

--- src/obsidian/augmentations/CliData.d.ts
+++ src/obsidian/augmentations/CliData.d.ts
@@ -5,7 +5,10 @@ declare module 'obsidian' {
      * The data passed to the CLI.
      */
     export interface CliData {
-        /** @official */
-        [key: string]: string | 'true';
+        /**
+         * @official
+         * @deprecated - Added only for typing purposes. Use `this[key]` instead.
+         */
+        index__(key: string): string | 'true';
     }
 }
