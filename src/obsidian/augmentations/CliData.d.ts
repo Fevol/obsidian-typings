6ea53ac9949ad14f941594aa21199590988684db
Michael Naumov <mnaoumov@gmail.com>
2026-02-24 20:34:27 -0600

    feat: update API changes

--- src/obsidian/augmentations/CliData.d.ts
+++ src/obsidian/augmentations/CliData.d.ts
@@ -3,6 +3,8 @@ export {};
 declare module 'obsidian' {
     /**
      * The data passed to the CLI.
+     *
+     * @since 1.12.2
      */
     export interface CliData extends Record<string, string | 'true'> {
     }
--- src/obsidian/augmentations/CliFlags.d.ts
+++ src/obsidian/augmentations/CliFlags.d.ts
@@ -5,6 +5,7 @@ declare module 'obsidian' {
      * A map of CLI flags.
      *
      * @deprecated - Added only for typing purposes. Use {@link CliFlag} instead.
+     * @since 1.12.2
      */
     type CliFlags__ = Record<string, CliFlag>;
 }
