977e9bbc1aebc513fee54fe9f1bc34d384de671b
Michael Naumov <mnaoumov@gmail.com>
2026-03-11 17:33:49 -0600

    chore: format scripts

--- scripts/format.ts
+++ scripts/format.ts
@@ -1,4 +1,4 @@
 import { spawnSync } from 'child_process';
 
-const fmt = spawnSync('dprint fmt', { stdio: 'inherit', shell: true });
-process.exit(fmt.status ?? 1);
+const result = spawnSync('dprint fmt', { stdio: 'inherit', shell: true });
+process.exit(result.status ?? 1);
