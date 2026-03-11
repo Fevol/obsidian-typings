ad90712d7947f7b181e6f45b2d2fdcb192baa823
Michael Naumov <mnaoumov@gmail.com>
2026-03-11 15:47:41 -0600

    chore: remove linting from format script

--- scripts/format.ts
+++ scripts/format.ts
@@ -1,9 +1,4 @@
 import { spawnSync } from 'child_process';
 
-const lint = spawnSync('eslint --fix', { stdio: 'inherit', shell: true });
-if (lint.status !== 0) {
-  process.exit(lint.status ?? 1);
-}
-
 const fmt = spawnSync('dprint fmt', { stdio: 'inherit', shell: true });
 process.exit(fmt.status ?? 1);
