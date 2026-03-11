b9d6ce231b79b91b6f6a547ce1f4e6ae80d53fef
Michael Naumov <mnaoumov@gmail.com>
2026-03-11 17:58:54 -0600

    chore: ensure no aliases

--- scripts/build-fix-bundle-types.ts
+++ scripts/build-fix-bundle-types.ts
@@ -1,11 +1,3 @@
-import {
-  readFile,
-  writeFile
-} from 'node:fs/promises';
+import { fixBundleTypes } from './helpers/fix-bundle-types.ts';
 
-const DIST_TYPES_FILE = 'dist/cjs/types.d.cts';
-const content = await readFile(DIST_TYPES_FILE, 'utf-8');
-const newContent = content
-  .replace(/Plugin\$1/g, 'Plugin')
-  .replace(/import \{/g, 'import type {');
-await writeFile(DIST_TYPES_FILE, newContent, 'utf-8');
+await fixBundleTypes('dist/cjs/types.d.cts');
