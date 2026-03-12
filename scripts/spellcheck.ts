90835284c6aedd6f59a1d799cdc1bfe85699f44a
Michael Naumov <mnaoumov@gmail.com>
2026-03-11 20:51:32 -0600

    chore: spellcheck only staged files

--- scripts/spellcheck.ts
+++ scripts/spellcheck.ts
@@ -1,3 +1,36 @@
+import type { Glob } from '@cspell/cspell-types';
+
+import {
+  checkFilenameMatchesGlob,
+  isBinaryFile,
+  loadConfig
+} from 'cspell-lib';
+
 import { execFromRoot } from './helpers/exec.ts';
 
-await execFromRoot('cspell . --no-progress');
+async function main() {
+  const [, , ...paths] = process.argv;
+  await spellcheck(paths);
+}
+
+export async function spellcheck(paths: string[] = []): Promise<void> {
+  let filteredPaths = await filterPaths(paths);
+  if (filteredPaths.length === 0) {
+    if (paths.length === 0) {
+      filteredPaths = ['.'];
+    } else {
+      return;
+    }
+  }
+  await execFromRoot(['npx', 'cspell', ...filteredPaths, '--no-progress']);
+}
+
+async function filterPaths(paths: string[]): Promise<string[]> {
+  const config = await loadConfig('cspell.json');
+  const ignorePaths = config.ignorePaths ?? [];
+  const globs = ignorePaths.map((p: Glob): string => typeof p === 'string' ? p : p.glob);
+
+  return paths.filter((path) => !isBinaryFile(path) && !checkFilenameMatchesGlob(path, globs));
+}
+
+await main();
