7db51dd930f47731a4eaafd98c9de2cdbfdf87c4
Michael Naumov <mnaoumov@gmail.com>
2026-03-12 10:16:36 -0600

    fix: generator added $ types

--- scripts/helpers/fix-bundle-types.ts
+++ scripts/helpers/fix-bundle-types.ts
@@ -6,15 +6,8 @@ import {
 export async function fixBundleTypes(bundleTypesFilePath: string): Promise<void> {
   const content = await readFile(bundleTypesFilePath, 'utf-8');
   const newContent = content
-    .replace(/Plugin\$1/g, 'Plugin')
+    .replace(/\$\d+/g, '')
     .replace(/import \{/g, 'import type {');
-
-  const ALIASED_TYPES_REGEXP = /\w+\$\d+/g;
-  const aliasedTypes = Array.from(newContent.matchAll(ALIASED_TYPES_REGEXP)).map((match) => match[0]);
-
-  if (aliasedTypes.length > 0) {
-    throw new Error(`Generated aliases found in ${bundleTypesFilePath}: ${aliasedTypes.join(', ')}`);
-  }
 
   await writeFile(bundleTypesFilePath, newContent, 'utf-8');
 }
